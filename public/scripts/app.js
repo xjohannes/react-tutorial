ae98e4004c8815d3385d3d8a3ed9b7b4777f8760
Johannes Akse <johannes.akse@knowit.no>
2016-09-01 16:45:08 +0200

    Staging all files

---
+++ public/scripts/app.js
@@ -0,0 +1,15 @@
+var CommentBox = React.createClass({
+  render: function () {
+    return (
+      <div className="commentBox">
+        Hello, world! I am a CommentBox.
+      </div>
+    )
+  }
+});
+
+ReactDOM.render(
+  <CommentBox />,
+  document.getElementById('content')
+);
+
